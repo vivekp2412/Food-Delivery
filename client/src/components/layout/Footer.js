9a672a76b8de2cbadb7e5822863cc3787b61bef7
jupinlathiya <jupinlathiya008@gmail.com>
2023-04-24 22:49:47 +0530

    added

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer grey darken-4">
+      <div className="container white-text">
+        <div className="left">
+          <h6></h6>
+        </div>
+        <div className="right">
+          <h6>
+            Made by{" "}
+            <a href="https://github.com/harshitkathuria" target="_blank">
+              <span className="blue-text">❤️</span>
+            </a>
+          </h6>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
